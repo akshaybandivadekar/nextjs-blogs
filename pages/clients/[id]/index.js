5c6b40c0eb785db2c0e9e016a6201bbf66eb9d43
akshaybandivadekar <c4.akshay.bandivadekar@gmail.com>
2022-05-18 15:42:39 +0530

    Navigating Programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,21 @@ function ClientProjectPage() {
   const router = useRouter();
   console.log(router.query);
 
+  function loadProjectHandler() {
+    //load data
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: "akshay",
+        clientprojectid: "projecta",
+      },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
