f7634da98d757f6bfdb71bc5aad3861474212efc
akshaybandivadekar <c4.akshay.bandivadekar@gmail.com>
2022-05-18 15:16:32 +0530

    Navigating to Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "akshay", name: "akshay" },
+    { id: "sachin", name: "sachin" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
