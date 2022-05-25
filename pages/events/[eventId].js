213d8b07ea03e1733497bcbb8107d5878b03418b
akshaybandivadekar <c4.akshay.bandivadekar@gmail.com>
2022-05-25 14:52:48 +0530

    Optimizing Data Fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -3,15 +3,14 @@ import { Fragment } from 'react';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistic from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
-import ErrorAlert from '../../components/ui/error-alert';
-import { getAllEvents, getEventById } from '../../helpers/api-util';
+import { getFeaturedEvents, getEventById } from '../../helpers/api-util';
 
 function EventDetailPage({ event }) {
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No Event Found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -45,11 +44,12 @@ export async function getStaticProps(context) {
     props: {
       event,
     },
+    revalidate: 30,
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
 
   return {
