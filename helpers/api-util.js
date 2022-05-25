459814af1431edf261bd4c6a63f0cba7f6afb0c9
akshaybandivadekar <c4.akshay.bandivadekar@gmail.com>
2022-05-25 13:24:27 +0530

    Adding Static Site Generation(SSG) on Home Page

---
+++ helpers/api-util.js
@@ -0,0 +1,19 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    'https://nextjs-blogs-9a231-default-rtdb.firebaseio.com/events.json'
+  );
+  const data = await response.json();
+  const events = [];
+
+  for (let key in data) {
+    events.push({ id: key, ...data[key] });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+
+  return allEvents.filter((event) => event.isFeatured);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
-import { getFeaturedEvents } from "../dummy-data";
-import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
 
-function HomePage() {
-  const featureEvents = getFeaturedEvents();
+function HomePage(props) {
   return (
     <div>
-      <EventList items={featureEvents}/>
+      <EventList items={props.featureEvents} />
     </div>
   );
 }
 
 export default HomePage;
+
+export async function getStaticProps() {
+  const featureEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      featureEvents,
+    },
+  };
+}
